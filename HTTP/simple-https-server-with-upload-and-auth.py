f6357590fb8e01d804445a359f66495be2f23f5d
FBakri <93790223+offport@users.noreply.github.com>
2025-03-12 03:23:29 -0700

    Update simple-https-server-with-upload-and-auth.py

--- HTTP/simple-https-server-with-upload-and-auth.py
+++ HTTP/simple-https-server-with-upload-and-auth.py
@@ -60,7 +60,7 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 f.close()
             pass
         else:
-            print self.headers.getheader('Authorization')
+            print(self.headers.getheader('Authorization'))
             self.do_AUTHHEAD()
             self.wfile.write(self.headers.getheader('Authorization'))
             self.wfile.write('not authenticated')
@@ -68,7 +68,7 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         
 
     def do_HEAD(self):
-        print "send header"
+        print("send header")
         self.send_response(401)
         self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
         self.send_header('Content-type', 'text/html')
@@ -314,8 +314,8 @@ def test(HandlerClass = SimpleHTTPRequestHandler,
 
 if __name__ == '__main__':
     if len(sys.argv)<3:
-        print "usage SimpleAuthServer.py [port] [username:password]"
+        print("usage SimpleAuthServer.py [port] [username:password]")
         sys.exit()
     key = base64.b64encode(sys.argv[2])
-    print key
+    print(key)
     test()
